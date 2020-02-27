c2da7dd636b11252b1bf784385b5bcccd08ea179
Muhammed Kalender <muhammedkalender@protonmail.com>
2020-02-27 08:51:07 +0300

    - Soft Input Force Hide - Soft Input Action - Bazı Sayfalar Scroll View içine taşındı ( Password, New Password, Settings )

--- app/src/main/java/com/muhammedkalender/pocketpassword/Helpers/SystemHelper.java
+++ app/src/main/java/com/muhammedkalender/pocketpassword/Helpers/SystemHelper.java
@@ -1,7 +1,9 @@
 package com.muhammedkalender.pocketpassword.Helpers;
 
 import android.app.Activity;
+import android.content.Context;
 import android.inputmethodservice.InputMethodService;
+import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 
 import com.muhammedkalender.pocketpassword.Constants.ErrorCodeConstants;
@@ -12,8 +14,11 @@ public class SystemHelper {
     //https://stackoverflow.com/a/9494042
     public void hideSoftKeyboard() {
         try {
-            if (((InputMethodManager) Global.CONTEXT.getSystemService(Activity.INPUT_METHOD_SERVICE)).isAcceptingText()) {
-                ((InputMethodManager) Global.CONTEXT.getSystemService(Activity.INPUT_METHOD_SERVICE)).toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+            View view = Global.ACTIVITY.findViewById(android.R.id.content);
+
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager) Global.ACTIVITY.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
             }
         } catch (Exception e) {
             Helpers.logger.error(ErrorCodeConstants.SYSTEM_SERVICE_HIDE_SOFT_KEYBOARD, e);
